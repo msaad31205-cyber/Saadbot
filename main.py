a5d535292535d7f5de4a24e5e0c40269d054ab56
msaad31205-cyber <msaad31205@gmail.com>
2026-04-27 01:27:50 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Saad Bot Working 🤖")
+
+while True:
+    user = input("You: ")
+
+    if user == "bye":
+        print("Bot: Allah Hafiz 👋")
+        break
+
+    print("Bot: Tum ne kaha ->", user)
